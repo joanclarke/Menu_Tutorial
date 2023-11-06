1b36c60d32e1bf44e662b9da0ddc48a00a6149b4
joanclarke <jcjuju2014@gmail.com>
2023-11-06 17:26:49 -0500

    add category headings including the option 'all'

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import Title from './Title'
 import menu from './data'
 import Menu from './Menu'
+import Categories from './Categories'
+
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))]
 
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu)
+  const [categories, setCategories] = useState(allCategories)
+
   return (
     <main>
       <section className="menu">
         <Title text="our menu" />
+        <Categories categories={categories} />
         <Menu items={menuItems} />
       </section>
     </main>
